c9484deaf25db9d6eea84b32e26324ade1d573ed
Hans Yadiel Sanchez Mora <hanssanchez427@gmail.com>
2023-10-13 22:51:16 -0500

    Estructura de carpetas

---
+++ src/main/java/com/api/springbootbaseapi/SpringBootBaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.springbootbaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/springbootbaseapi/SpringBootBaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.springbootbaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
